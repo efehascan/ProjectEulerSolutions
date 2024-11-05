ecea6f98e570916f5cca464b0dcad4878c512767
Efe Hascan <82836263+efehascan@users.noreply.github.com>
2024-11-05 15:05:56 +0300

    Add files via upload

---
+++ (2)EvenFibonacciNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Sınır giriniz: ");
+int sinir = Convert.ToInt32(Console.ReadLine());
+
+int toplam = 1;
+int cikti = 0;
+
+
+for (int i = 2; i < sinir; i+= toplam)
+{
+    Console.WriteLine("Birinci sayı:" + toplam +"  ||  "+ "İkinci sayı:" + i +"  ||  "+ "Çift sayılar toplamı:" + cikti);
+    
+    toplam += i;
+    
+    if (toplam % 2 == 0){
+        cikti += toplam;
+    }else if (i % 2 == 0)
+    {
+        cikti += i;
+    }
+}
+Console.WriteLine("Çift sayılar toplamı: " +cikti);
+
+// 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
