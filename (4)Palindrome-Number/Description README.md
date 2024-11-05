b9c4c5cc866830b41eb0f151e6b16af6ac931f98
Efe Hascan <82836263+efehascan@users.noreply.github.com>
2024-11-05 15:14:54 +0300

    Add files via upload

---
+++ (4)Palindrome-Number/Description README.md
@@ -0,0 +1,13 @@
+Türkçe Açıklama
+Bu proje, "Largest Palindrome Product" adlı problemde verilen görevi çözmek için geliştirilmiştir.
+Palindromik bir sayı, her iki yönde de aynı şekilde okunan sayıdır.
+İki basamaklı iki sayının çarpımından elde edilen en büyük palindrom 9009 olup, 91 ve 99’un çarpımıdır.
+Görevde, üç basamaklı iki sayının çarpımından elde edilen en büyük palindromun bulunması istenmektedir.
+Bu projede, üç basamaklı sayılar arasındaki en büyük palindromik çarpımı bulan bir algoritma ile çözüm sağlanmıştır.
+
+English Description
+This project was developed to solve the "Largest Palindrome Product" problem.
+A palindromic number reads the same both ways.
+The largest palindrome made from the product of two 2-digit numbers is 9009, which is the product of 91 and 99.
+The goal is to find the largest palindrome made from the product of two 3-digit numbers.
+In this project, we implemented an algorithm to find the largest palindromic product of two 3-digit numbers.
---
+++ (4)Palindrome-Number/Program.cs
@@ -0,0 +1,78 @@
+﻿Console.Write("Sınır sayısı girin: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Hangi sayıya kadar çarparak ilerlesin?");
+Console.Write("A: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("B: ");
+int b = int.Parse(Console.ReadLine());
+
+List<int> palindromlar = new List<int>();
+List<int> palindromCarpmali = new List<int>();
+
+for (int i = 1; i <= x; i++)
+{
+    if (i % 10 == 0 && i != 0)
+    {
+        // Bütün denenen değerleri görmek isterseniz aşağıdaki kodu etkinleştirin
+        // if you want to see all of values, you must activate the code below
+        // Console.WriteLine($"i: {i} - False"); 
+        continue;
+    }
+
+    int number = 0;
+    int gecicidegisken = i;
+
+    while (gecicidegisken > 0)
+    {
+        number = number * 10 + gecicidegisken % 10;
+        gecicidegisken /= 10;
+    }
+    if (number == i)
+    {
+        Console.WriteLine($"i: {i} - Palindrom");
+        palindromlar.Add(i);
+    }
+}
+
+for (int i = 0; i <= a; i++)
+{
+    for (int j = 0; j <= b; j++)
+    {
+        int carpim = i * j;
+        if (kontrol(carpim))
+        {
+            Console.WriteLine($"Palindrom: {carpim} = {i} x {j}");
+            palindromCarpmali.Add(carpim);
+        }
+    }
+}
+
+if (palindromCarpmali.Any())
+{
+    int palindromMax = palindromCarpmali.Max();
+    Console.WriteLine($"En büyük Çarpmalı Palindrom: {palindromMax}");
+}else{
+    Console.WriteLine("Hiç çarpmalı palindrom bulunamadı.");
+}
+
+if (palindromlar.Any())
+{
+    int palindromMax = palindromlar.Max();
+    Console.WriteLine($"En büyük Palindrom: {palindromMax}");
+} else {
+    Console.WriteLine("Hiç palindrom bulunamadı.");
+}
+
+bool kontrol(int sayi)
+{
+    int orjinal = sayi;
+    int ters = 0;
+    while (sayi > 0)
+    {
+        ters = ters * 10 + sayi % 10;
+        sayi /= 10;
+    }
+    
+    return orjinal == ters;
+}
