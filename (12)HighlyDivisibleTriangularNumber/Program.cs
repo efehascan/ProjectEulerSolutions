a820ce4e0929e172d52ba99e2d1d0972bc17cfeb
Efe Hascan <82836263+efehascan@users.noreply.github.com>
2024-11-07 21:00:35 +0300

    Add files via upload

---
+++ (12)HighlyDivisibleTriangularNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Highest Divisible Triangular Number");
+
+
+int n = 1;
+int CokcaBolunenDeger = 0;
+
+while (true)
+{
+    CokcaBolunenDeger = (n * (n + 1)) / 2;
+    int bolensayisi = BolenSayisi(CokcaBolunenDeger);
+
+    if (bolensayisi > 500)
+    {
+        Console.WriteLine(CokcaBolunenDeger);
+        break;
+    }
+
+    n++;
+}
+
+static int BolenSayisi(int n)
+{
+    int sayac = 0;
+
+    for (int i = 1; i <= Math.Sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            if (n / i == i)
+                sayac++;
+            else
+                sayac += 2;
+        }
+    }
+
+    return sayac;
+}
